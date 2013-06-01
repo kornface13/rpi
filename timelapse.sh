e8e7779354cdae2ecbb4e994986bbeac5b3ebb67
kornface13 <kornface13@gmail.com>
2013-06-01 01:35:38 -0300

    Create timelapse.sh

---
+++ timelapse.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Simple script to help take timelapse photos with the rPI camera module
+
+num_one=0
+num_two=0
+
+echo "You should run this inside a screen session to avoid aborting the program if you lose your SSH session."
+echo -n "How often would you like a photo to be taken (in seconds): "
+read num_one
+echo -n "How long would you like the time lapse to run (in minutes): "
+read num_two
+
+often=$((num_one * 1000))
+duration=$((num_two * 1000 * 60))
+
+echo "$often"
+echo "$duration"
+
+echo -e "\n$(((num_two * 60) / num_one)) pictures will be taken\n\n"
+echo -e "Starting now..."
+
+raspistill -o img%04d.jpg -tl $often -t $duration
+
+echo -e "\nAll done"
