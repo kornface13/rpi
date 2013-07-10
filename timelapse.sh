634e3a0824c09a6f9c60ba066d4f109d4f305caa
kornface13 <kornface13@gmail.com>
2013-07-10 14:45:49 -0400

    Revert "fuck it up"
    This reverts commit a0a91ca7ae0de0fb9dcea9616de9e2b72018cc23.

--- timelapse.sh
+++ timelapse.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 # Simple script to help take timelapse photos with the rPI camera module
 
+num_one=0
+num_two=0
+
+echo -e "\n\n"
+echo "* * * * * * * * * * * * * * * *"
+echo "* You should run this inside  *"
+echo "*  a screen session so the    *"
+echo "*  timelapse doesn't die if   *"
+echo "*  your SSH session closes    *"
+echo "* * * * * * * * * * * * * * * *"
+echo -en "\n\nHow often would you like a photo to be taken (in seconds): "
+read num_one
+echo -n "How long would you like the time lapse to run (in minutes): "
+read num_two
+
 often=$((num_one * 1000))
 duration=$((num_two * 1000 * 60))
 
